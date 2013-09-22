44f407fc1d87e104cd6f4b0ae4f2d44358cb0a42
Pierce Jensen <zaphodp.jensen@comcast.net>
2013-09-21 21:02:15 -0400

    CORNER BALL CONDITIONS. THAT IS ALL.

--- Entity.java
+++ Entity.java
@@ -42,6 +42,8 @@ public class Entity extends GameMechanics{
 	
 	Random generator;
 	
+	private final double BALL_ELASTICITY=1;
+	
 	Entity(){
 		generator = new Random();
 	}
@@ -134,10 +136,12 @@ public class Entity extends GameMechanics{
 		
 
 		//map bound handlers
-		int leftBound = -1000;
-		int topBound = 5720;
-		int bottomBound = 280;
-		int rightBound = 7000;
+		final int leftBound = -1000;
+		final int topBound = 5720;
+		final int bottomBound = 280;
+		final int rightBound = 7000;
+		final int GOAL_TOP= 3430;
+		final int GOAL_BOTTOM= 2590;
 		
 		if(this.type == 0){//player
 			if(this.x < leftBound + this.size){ //left-right map bound stopper
@@ -159,7 +163,7 @@ public class Entity extends GameMechanics{
 		//3430 2590
 		if(this.type == 1){//ball
 			if(this.x < leftBound + this.size){ //left-right map bound stopper
-				if(this.y < 3430-this.size && this.y > 2590+this.size){//if it's in a goal
+				if(this.y < GOAL_TOP-this.size && this.y > GOAL_BOTTOM+this.size){//if it's in a goal
 					if(goalPosition == BLUE_GOAL_RIGHT){
 						redScore += 1;
 					}else{
@@ -174,7 +178,7 @@ public class Entity extends GameMechanics{
 					this.vx *= -.75;
 				}
 			}else if(this.x > rightBound - this.size){
-				if(this.y < 3430-this.size && this.y > 2590+this.size){
+				if(this.y < GOAL_TOP-this.size && this.y > GOAL_BOTTOM+this.size){
 					if(goalPosition == BLUE_GOAL_RIGHT){
 						blueScore += 1;
 					} else {
@@ -186,7 +190,7 @@ public class Entity extends GameMechanics{
 					this.vy = 0;
 				} else {
 					this.x = rightBound - this.size;
-					this.vx *= -.75;
+					this.vx *= -BALL_ELASTICITY;
 				}
 			}
 			
@@ -197,6 +201,43 @@ public class Entity extends GameMechanics{
 				this.y = bottomBound + this.size;
 				this.vy *= -1;
 			}
+			//CORNER CONDITIONS
+			final double CORNER_POSITION_OFFSET_MULTIPLIER = 1.1*.7071;//NEEDS *.7071 FOR PROPER OPERATION
+			if(this.y<(-this.x)+100+(.7071*this.size))//Bottom Left
+			{
+				double temp= -(this.vx*BALL_ELASTICITY);
+				this.vx=-(this.vy*BALL_ELASTICITY);
+				this.vy=temp;
+				
+				this.x+=CORNER_POSITION_OFFSET_MULTIPLIER*this.size;
+				this.y+=CORNER_POSITION_OFFSET_MULTIPLIER*this.size;
+			}
+			if(this.y>-(this.x)+11900+(.7071*this.size))//Top Right
+			{
+				double temp= -(this.vx*BALL_ELASTICITY);
+				this.vx=-(this.vy*BALL_ELASTICITY);
+				this.vy=temp;
+				this.x-=CORNER_POSITION_OFFSET_MULTIPLIER*this.size;
+				this.y-=CORNER_POSITION_OFFSET_MULTIPLIER*this.size;
+			}
+			
+			if(this.y>(this.x)+5900+(.7071*this.size))//Top Left
+			{
+				double temp= (this.vx*BALL_ELASTICITY);
+				this.vx=(this.vy*BALL_ELASTICITY);
+				this.vy=temp;
+				this.x+=CORNER_POSITION_OFFSET_MULTIPLIER*this.size;
+				this.y-=CORNER_POSITION_OFFSET_MULTIPLIER*this.size;
+			}
+			if( this.y<(this.x)-5900+(.7071*this.size))//Bottom Right
+			{
+				double temp= (this.vx*BALL_ELASTICITY);
+				this.vx=(this.vy*BALL_ELASTICITY);
+				this.vy=temp;
+				this.x-=CORNER_POSITION_OFFSET_MULTIPLIER*this.size;
+				this.y+=CORNER_POSITION_OFFSET_MULTIPLIER*this.size;
+			}
+			
 		}
 		
 		return false;
