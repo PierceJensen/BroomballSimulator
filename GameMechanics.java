10b08a612a8750f83bbcad869b8f4d7d5d4aadff
PierceJensen <zaphodp.jensen@comcast.net>
2013-10-21 11:54:29 -0400

    Fixed some stuff, still broken.

--- GameMechanics.java
+++ GameMechanics.java
@@ -136,15 +136,17 @@ public class GameMechanics {
 			corner[i].ypoints = cornerPointsY[i];
 		}
 
-		int[] pointsX = {500,500,1000,1000};
-		int[] pointsY = {0,1000,1000,0};
 		final int BOUNDARY_SIZE = 100;
-		pushOffBoundary = new Polygon();
-		pushOffBoundary.npoints=8;
 		
 		float pushCosOffset = BOUNDARY_SIZE*cos[67];
 		float pushSinOffset = BOUNDARY_SIZE*sin[67];
-			pushOffBoundary.addPoint((int)( cornerPointsX[0][0]-pushCosOffset),(int)(  cornerPointsY[0][0]-pushSinOffset));
+	
+		float[] pointsX = {cornerPointsX[0][0]-pushSinOffset,cornerPointsX[0][2]-pushCosOffset,cornerPointsX[1][0]+pushCosOffset,cornerPointsX[1][2]+pushSinOffset,cornerPointsX[2][0]+pushSinOffset,cornerPointsX[2][2]+pushCosOffset,cornerPointsX[3][0]+pushCosOffset,cornerPointsX[3][2]-pushSinOffset};
+		float[] pointsY = {0,1000,1000,0};
+		pushOffBoundary = new Polygon();
+
+		
+		/*	pushOffBoundary.addPoint((int)( cornerPointsX[0][0]-pushCosOffset),(int)(  cornerPointsY[0][0]-pushSinOffset));
 			pushOffBoundary.addPoint((int)( cornerPointsX[0][2]-pushSinOffset),(int)(  cornerPointsY[0][2]-pushCosOffset));
 		
 			pushOffBoundary.addPoint((int)( cornerPointsX[1][0]+pushCosOffset),(int)(  cornerPointsY[1][0]-pushSinOffset));
@@ -154,11 +156,11 @@ public class GameMechanics {
 			pushOffBoundary.addPoint((int)( cornerPointsX[2][2]+pushSinOffset),(int)(  cornerPointsY[2][2]+pushCosOffset));
 		
 			pushOffBoundary.addPoint((int)( cornerPointsX[3][0]-pushCosOffset),(int)(  cornerPointsY[3][0]+pushSinOffset));
-			pushOffBoundary.addPoint((int)( cornerPointsX[3][2]-pushSinOffset),(int)(  cornerPointsY[3][2]+pushCosOffset));
+			pushOffBoundary.addPoint((int)( cornerPointsX[3][2]-pushSinOffset),(int)(  cornerPointsY[3][2]+pushCosOffset));*/
 		
-		//pushOffBoundary.npoints=pointsX.length;
-		//pushOffBoundary.xpoints=pointsX;
-		//pushOffBoundary.ypoints=pointsY;
+		pushOffBoundary.npoints=pointsX.length;
+		pushOffBoundary.xpoints=pointsX;
+		pushOffBoundary.ypoints=pointsY;
 		
 
 		//player init
